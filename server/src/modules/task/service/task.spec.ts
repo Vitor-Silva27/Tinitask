12e2fc53bbbb79642def921ee14f0c6b85280ff7
Vitor-Silva27 <developer.vitorsilva@gmail.com>
2026-01-26 10:25:43 -0300

    test(test): :sparkles: update task service tests to validate task creation

--- server/src/modules/task/service/task.spec.ts
+++ server/src/modules/task/service/task.spec.ts
@@ -1,6 +1,31 @@
-it("should sum two numbers", () => {
-    const a = 5;
-    const b = 3;
-    const sum = a + b;
-    expect(sum).toBe(8);
+import { CreateTaskDTO } from "../dto/createTask.dto";
+import { ITaskRepository } from "../interface/ItaskRepository";
+import { ITaskService } from "../interface/ITaskService";
+import { TaskService } from "./task.services";
+
+const taskRepositoryMock: jest.Mocked<ITaskRepository> = {
+  createTask: jest.fn(),
+};
+
+describe("TaskService", () => {
+  let service: ITaskService;
+
+  beforeEach(() => {
+    service = new TaskService(taskRepositoryMock);
+  });
+
+  it("should create a task and return its id", async () => {
+    const dto: CreateTaskDTO = {
+      title: "Test Task",
+      description: "This is a test task",
+    };
+
+    taskRepositoryMock.createTask.mockResolvedValue("task-id-123");
+
+    const result = await service.createTask(dto);
+
+    expect(taskRepositoryMock.createTask).toHaveBeenCalledTimes(1);
+    expect(taskRepositoryMock.createTask).toHaveBeenCalledWith(dto);
+    expect(result).toBe("task-id-123");
+  });
 });
